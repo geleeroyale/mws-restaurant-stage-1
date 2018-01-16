cd01a0fe46f77e6fb1c9b3bcb5c31ba5a8759b8d
geleeroyale <krassvs@gmail.com>
2018-01-16 19:39:10 +0100

    fixed url error

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,7 +8,6 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 80 // Change this to your server port
     return `https://geleeroyale.github.io/mws-restaurant-stage-1/data/restaurants.json`;
   }
 
@@ -150,7 +149,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`./img/${restaurant.photograph}`);
   }
 
   /**
